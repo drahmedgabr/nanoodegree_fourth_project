0bb43b48fdde9a9de25a2855d8e1eff67270eb47
drahmedgabr <dr.ahmedgabr@yahoo.com>
2022-11-04 11:01:02 +0200

    remindersVM test

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,74 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.util.MainCoroutineRule
+import com.udacity.project4.locationreminders.util.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
+@Config(sdk = [28])
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private lateinit var remindersListViewModel: RemindersListViewModel
+    private lateinit var fakeDataSource: FakeDataSource
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        fakeDataSource = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(), fakeDataSource
+        )
+    }
+
+    @Test
+    fun remindersListViewModel_getListOfReminders() = runTest {
+        Dispatchers.setMain(StandardTestDispatcher())
+
+        //Given
+        remindersListViewModel.loadReminders()
+
+        //When
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(true))
+        advanceUntilIdle()
+
+        //Then
+        assertThat(remindersListViewModel.showLoading.getOrAwaitValue(), `is`(false))
+    }
+
+    @Test
+    fun remindersListViewModel_checkWhenError() {
+        //Given
+        fakeDataSource.shouldReturnError(true)
+
+        //When
+        remindersListViewModel.loadReminders()
+
+        //Then
+        assertThat(remindersListViewModel.showSnackBar.getOrAwaitValue(), not(nullValue()))
+    }
 }
