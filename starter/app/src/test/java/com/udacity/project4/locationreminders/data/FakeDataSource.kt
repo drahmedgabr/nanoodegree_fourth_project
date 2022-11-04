2fb66dd714a22a82b643c19fc054ee7b16bd9aa7
drahmedgabr <dr.ahmedgabr@yahoo.com>
2022-11-04 09:47:30 +0200

    Update FakeDataSource.kt

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,26 +3,39 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private val linkedHashMap: LinkedHashMap<String, ReminderDTO> = LinkedHashMap()
+    private var errorFound = false
+
+    fun shouldReturnError(e: Boolean) {
+        errorFound = e
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (errorFound) {
+            return Result.Error("Error while testing: getReminders()")
+        } else {
+            return Result.Success(linkedHashMap.values.toList())
+        }
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        linkedHashMap[reminder.id] = reminder
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (errorFound) {
+            return Result.Error("Error while testing: getReminders(id: String)")
+        } else {
+            linkedHashMap[id]?.let {
+                return Result.Success(it)
+            }
+            return Result.Error("Error while testing: Reminder Not found")
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        linkedHashMap.clear()
     }
-
-
 }
